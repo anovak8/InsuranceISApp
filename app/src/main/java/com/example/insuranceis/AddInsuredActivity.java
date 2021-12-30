77b84a340d543aa498516ab9ee0ac6e7f6a4107b
anzo8 <novak.anze1@gmail.com>
2021-12-31 00:09:35 +0100

    Post add client, get Policy

---
+++ app/src/main/java/com/example/insuranceis/AddInsuredActivity.java
@@ -0,0 +1,116 @@
+package com.example.insuranceis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.UnsupportedEncodingException;
+
+public class AddInsuredActivity extends AppCompatActivity {
+
+    private TextView status;
+    private EditText name;
+    private EditText surname;
+    private EditText address;
+    private EditText zipCode;
+    private EditText city;
+
+    private RequestQueue requestQueue;
+    private String url = "https://insurance-is-dev.azurewebsites.net/api/v1/Insured";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_insured);
+
+        name = (EditText) findViewById(R.id.teName);
+        surname = (EditText) findViewById(R.id.teSurname);
+        address = (EditText) findViewById(R.id.teAddress);
+        zipCode = (EditText) findViewById(R.id.teZipCode);
+        status = (TextView) findViewById(R.id.status);
+        city = (EditText) findViewById(R.id.teCity);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+    }
+
+    public void addInsured(View view){
+        this.status.setText("Posting to " + url);
+        try{
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("firstMidName", name.getText());
+            jsonBody.put("lastName", surname.getText());
+            jsonBody.put("address", address.getText());
+            jsonBody.put("zipCode", zipCode.getText());
+            jsonBody.put("city", city.getText());
+
+            final String mRequestBody = jsonBody.toString();
+
+            status.setText(mRequestBody);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("LOG_VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("LOG_VOLLEY", error.toString());
+                }
+            }
+            ) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                        return null;
+                    }
+                }
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        status.setText(responseString);
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+
+            };
+
+            requestQueue.add(stringRequest);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- app/src/main/java/com/example/insuranceis/MainActivity.java
+++ app/src/main/java/com/example/insuranceis/MainActivity.java
@@ -23,13 +23,28 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.security.Policy;
 import java.util.ArrayList;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.insuranceis.MESSAGE";
+
+    public void addStudentActivity (View view) {
+        Intent intent = new Intent(this,AddInsuredActivity.class);
+        String message = "Add client.";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
     private RequestQueue requestQueue;
     private TextView Insured;
-    private String url = "https://insurance-is-dev.azurewebsites.net/api/v1/Insured";
+    private TextView Policy;
+    private String urlInsured = "https://insurance-is-dev.azurewebsites.net/api/v1/Insured";
+    private String urlPolicy = "https://insurance-is-dev.azurewebsites.net/api/v1/InsurancePolicy";
 
 
     @Override
@@ -38,19 +53,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         requestQueue = Volley.newRequestQueue(getApplicationContext());
         Insured = (TextView) findViewById(R.id.Insured);
+        Policy = (TextView) findViewById(R.id.Policy);
     }
 
     public void prikaziInsured(View view){
         if (view != null){
-            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            JsonArrayRequest request = new JsonArrayRequest(urlInsured, insuredArrayListener, errorListener);
             requestQueue.add(request);
         }
     }
 
-    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+    public void prikaziPolicy(View view){
+        if (view != null){
+            JsonArrayRequest request = new JsonArrayRequest(urlPolicy, policyArrayListener, errorListener);
+            requestQueue.add(request);
+        }
+    }
+
+    private Response.Listener<JSONArray> insuredArrayListener = new Response.Listener<JSONArray>() {
         @Override
         public void onResponse(JSONArray response){
+            Policy.setVisibility(View.INVISIBLE);
+            Insured.setVisibility(View.VISIBLE);
             ArrayList<String> data = new ArrayList<>();
+
 
             for (int i = 0; i < response.length(); i++) {
                 try{
@@ -76,12 +102,47 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private Response.Listener<JSONArray> policyArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response){
+            Insured.setVisibility(View.INVISIBLE);
+            Policy.setVisibility(View.VISIBLE);
+            ArrayList<String> data = new ArrayList<>();
+
+            for (int i = 0; i < response.length(); i++) {
+                try{
+                    JSONObject object = response.getJSONObject(i);
+                    Integer policyID = object.getInt("insurancePolicyID");
+                    Integer cost = object.getInt("finalSum");
+                    String dateFrom = object.getString("dateFrom").split("T")[0];
+                    String dateTo = object.getString("dateTo").split("T")[0];
+                    Integer insuredID = object.getInt("insuredID");
+                    Integer insuranceSubjectID = object.getInt("insuranceSubjectID");
+                    Integer insuranceSubtypeID = object.getInt("insuranceSubtypeID");
+
+                    data.add(policyID + ", " + cost + "€ " + dateFrom + " to " + dateTo + "; " + insuredID + ", " + insuranceSubjectID + ", " + insuranceSubtypeID);
+                }catch (JSONException e){
+                    e.printStackTrace();
+                    return;
+                }
+            }
+
+            Policy.setText("");
+            for (String row: data){
+                String currentText = Policy.getText().toString();
+                Policy.setText(currentText + "\n\n" + row);
+
+            }
+        }
+    };
+
     private Response.ErrorListener errorListener = new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError error) {
             Log.d("REST error", error.getMessage());
         }
     };
+
 
 
 }
